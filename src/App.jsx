32b275d82340ff694bf3f602f78f905fb3388272
Reza Soltani <reza.soltani@gmail.com>
2023-11-02 14:13:28 +0330

    some minor modifications

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,6 @@ import { useState } from 'react'
 
 function App() {
   const [background, setBackground] = useState()
-  const [geolocation, setGeolocation] = useState({lat: '', lon: ''})
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else {
-    console.log("Geolocation not supported");
-  }
-  
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-  }
-  
-  function error() {
-    console.log("Unable to retrieve your location");
-  }
 
   return(
     <div className={`min-h-screen ${background}`} style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
